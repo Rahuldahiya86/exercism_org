170daa98320dfd5eef0b80d5a65ab8880bcccf3d
Rahuldahiya86 <rahuldahiya01235@gmail.com>
2024-10-22 16:41:11 +0530

    array_edit

--- array_ques_5/file.js
+++ array_ques_5/file.js
@@ -1,39 +1,39 @@
-// 1. Function to get an item from a specific position
+// 1. Item get kr liya uski position se
 export function getItem(stack, index) {
     return stack[index];
 }
 
-// 2. Function to replace an item at a specific position
+// 2. replace item 
 export function setItem(stack, index, replacementCard) {
     stack[index] = replacementCard;
     return stack;
 }
 
-// 3. Function to insert a new card at the top of the stack
+// 3. array ke last mein add kr diya new card
 export function insertItemAtTop(stack, newCard) {
     stack.push(newCard); 
     return stack; 
 }
 
-// 4. Function to remove an item from a specific position
+// 4. splice item remome
 export function removeItem(stack, index) {
     stack.splice(index, 1);
     return stack; 
 }
 
-// 5. Function to remove the item from the top of the stack
+// 5. last index ko delete kr diya
 export function removeItemFromTop(stack) {
     stack.pop(); 
     return stack; 
 }
 
-// 6. Function to insert a new card at the bottom of the stack
+// 6. add krdiya unshift se starting me
 export function insertItemAtBottom(stack, newCard) {
     stack.unshift(newCard);
     return stack; 
 }
 
-// 7. Function to remove the item at the bottom of the stack
+// 7. remove krdiya shift se starting me
 export function removeItemAtBottom(stack) {
     stack.shift();
     return stack; 
